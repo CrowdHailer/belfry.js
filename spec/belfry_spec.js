bb8884016e5ebc8f6edf227d182c92a72766e6c7
Peter <peterhsaxton@gmail.com>
2014-06-05 14:37:26 +0100

    refactored

--- spec/belfry_spec.js
+++ spec/belfry_spec.js
@@ -6,36 +6,52 @@ describe('tower', function(){
     dummy2 = jasmine.createSpy();
   });
 
-  it('should publish events to the subscribed handler', function(){
-    tower.subscribe('channel1')(dummy);
-    tower.publish('channel1')({x: 1});
-    expect(dummy).toHaveBeenCalledWith({x: 1}, 'channel1');
+  describe('setup', function(){
+    it('should create a singleton instance of tower', function(){
+      var otherTower = Belfry.getTower();
+      expect(otherTower).toBe(tower);
+    });
   });
 
-  it('should publish events to all the subscribed handlers', function(){
-    tower.subscribe('channel1')(dummy);
-    tower.subscribe('channel1')(dummy2);
-    tower.publish('channel1')({x: 1});
-    expect(dummy).toHaveBeenCalledWith({x: 1}, 'channel1');
-    expect(dummy2).toHaveBeenCalledWith({x: 1}, 'channel1');
+  describe('subscribing', function(){
+    it('should publish events to the subscribed handler', function(){
+      tower.subscribe('channel1')(dummy);
+      tower.publish('channel1')({x: 1});
+      expect(dummy).toHaveBeenCalledWith({x: 1}, 'channel1');
+    });
+
+    it('should publish events to all the subscribed handlers', function(){
+      tower.subscribe('channel1')(dummy);
+      tower.subscribe('channel1')(dummy2);
+      tower.publish('channel1')({x: 1});
+      expect(dummy).toHaveBeenCalledWith({x: 1}, 'channel1');
+      expect(dummy2).toHaveBeenCalledWith({x: 1}, 'channel1');
+    });
+
+    it('should return true if it has published to any listeners', function(){
+      tower.subscribe('channel1')(dummy);
+      var response = tower.publish('channel1')({x: 1});
+      expect(response).toBe(true);
+    });
+    
+    it('should respond false if there are no listeners', function(){
+      var response = tower.publish('channelEmpty')({x: 1});
+      expect(response).toBe(false);
+    });
+
+    it('should publish events to only the correct channel', function(){
+      tower.subscribe('channel1')(dummy);
+      tower.subscribe('channel2')(dummy2);
+      tower.publish('channel1')({x: 1});
+      expect(dummy).toHaveBeenCalledWith({x: 1}, 'channel1');
+      expect(dummy2).not.toHaveBeenCalledWith({x: 1}, 'channel1');
+      tower.publish('channel2')({x: 2});
+      expect(dummy).not.toHaveBeenCalledWith({x: 2}, 'channel2');
+      expect(dummy2).toHaveBeenCalledWith({x: 2}, 'channel2');
+    });
   });
 
-  it('should return true if it has published to any listeners', function(){
-    //option silent listeners
-    tower.subscribe('channel1')(dummy);
-    var response = tower.publish('channel1')({x: 1});
-    expect(response).toBe(true);
-  });
 
-  it('should respond false if there are no listeners', function(){
-    var response = tower.publish('channelEmpty')({x: 1});
-    expect(response).toBe(false);
-  });
-
-  it('should create a singleton instance of tower', function(){
-    var otherTower = Belfry.getTower();
-    expect(otherTower).toBe(tower);
-  });
 
   it('should return a unique identifier', function(){
     var uid1 = tower.subscribe('channel1')(dummy);
@@ -59,14 +75,4 @@ describe('tower', function(){
     expect(dummy2).not.toHaveBeenCalled();
   });
 
-  it('should publish events to only the correct channel', function(){
-    tower.subscribe('channel1')(dummy);
-    tower.subscribe('channel2')(dummy2);
-    tower.publish('channel1')({x: 1});
-    expect(dummy).toHaveBeenCalledWith({x: 1}, 'channel1');
-    expect(dummy2).not.toHaveBeenCalledWith({x: 1}, 'channel1');
-    tower.publish('channel2')({x: 2});
-    expect(dummy).not.toHaveBeenCalledWith({x: 2}, 'channel2');
-    expect(dummy2).toHaveBeenCalledWith({x: 2}, 'channel2');
-  });
 });
